d16feeba0b34a367c889b9a5f10edb35bc25919e
wechu07 <wechu017@gmail.com>
2023-07-10 09:10:45 +0300

    initialise postgres with admin

--- amayo/settings.py
+++ amayo/settings.py
@@ -88,10 +88,18 @@ WSGI_APPLICATION = 'amayo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'amayodb',
+        'USER': 'postgres',
+        'PASSWORD': 'nara1234',
+        'HOST': '127.0.0.1',
     }
 }
+
+# Admin
+# Username: amayo
+# email:test@amayo.com
+# password: nara1234
 
 
 # Password validation
