026a8f52a4029cf4896125c74cd51ad16d6648e6
wechu07 <wechu017@gmail.com>
2023-07-12 00:10:31 +0300

    update MCQ and SAQ models, with migration

--- papers/admin.py
+++ papers/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+from .models import Course, Level, Unit, Topic, Paper, MultipleChoiceQuestion, MultipleChoiceOption, ShortAnswerQuestion
 
-# Register your models here.
-from .models import Course, Paper #, Question
+class MultipleChoiceOptionInline(admin.TabularInline):
+    model = MultipleChoiceOption
+    extra = 3
+
+class MultipleChoiceQuestionAdmin(admin.ModelAdmin):
+    inlines = [MultipleChoiceOptionInline]
+    list_display = ["multiple_choice_question_text", "paper"]
 
 admin.site.register(Course)
-# admin.site.register(Paper)
-# admin.site.register(Question)
+admin.site.register(Level)
+admin.site.register(Unit)
+admin.site.register(Topic)
+admin.site.register(Paper)
+admin.site.register(MultipleChoiceQuestion, MultipleChoiceQuestionAdmin)
+admin.site.register(ShortAnswerQuestion)
--- papers/models.py
+++ papers/models.py
@@ -1,134 +1,140 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 # Q-bank segment
-
+# Course should reference the user
+# many to many
 class Course(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # User that created the course
     course_name = models.CharField(max_length=100, null=True)  # Name of the course
     description = models.TextField()  # Description of the course
 
     def __str__(self):
         return self.course_name
+class Level(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    level_name = models.CharField(max_length=100, null=True)  # Name of the level
+
+    def __str__(self):
+        return self.level_name
+
+class Unit(models.Model):
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, null=True)
+    unit_name = models.CharField(max_length=100, null=True)  # Name of the unit
+
+    def __str__(self):
+        return self.unit_name
+
+# Questions can be added linked to a specific topic, or a full paper
+# the models are defined below
+# they need to be linked to a specific course
+
+
 
 class Topic(models.Model):
     # Course that the topic belongs to
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
-    topic_name = models.CharField(max_length=100, null=True)  # Name of the topic
-    year_of_study = models.IntegerField()
-    class_unit = models.CharField(max_length=100)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, null=True)
+    topic_name = models.CharField(max_length=100, null=True)  # Name of the topic.
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.topic_name
-"example"
+
 class Paper(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, null=True)
     paper_title = models.CharField(max_length=100, null=True)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.paper_title
 
+class MultipleChoiceQuestion(models.Model):
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE, null=True)
+    # topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True)
+    multiple_choice_question_text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.multiple_choice_question_text
+
+class MultipleChoiceOption(models.Model):
+    multiple_choice_question = models.ForeignKey(MultipleChoiceQuestion, on_delete=models.CASCADE, null=True)
+    multiple_choice_option_text = models.CharField(max_length=200, default=None)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.multiple_choice_option_text
+
+class ShortAnswerQuestion(models.Model):
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True)
+    short_answer_question_text = models.TextField() # Text of the question
+    answer = models.TextField() # Answer to the question
+    discussion = models.TextField(blank=True) # Discussion or additional information about the question (optional)
+
+    def __str__(self):
+        return self.short_answer_question_text
+
 # class Question(models.Model):
-#     paper = models.ForeignKey(
-#         Paper, on_delete=models.CASCADE, related_name='questions')
-#     question_text = models.TextField()
+#     paper = models.ForeignKey(Paper, on_delete=models.CASCADE, null=True, blank=True)
+#     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True, blank=True)
+#     question_text = models.CharField(max_length=100, null=True)
 #     question_type = models.CharField(max_length=100, choices=[
 #         ('MCQ', 'Multiple Choice Question'),
 #         ('SAQ', 'Short Answer Question'),
 #         ('IVQ', 'Image/Video Question'),
 #     ])  # Type of the question (Multiple Choice, Short Answer, or Image/Video)
 
-#     def __str__(self):
-#         return self.question_text
+#     def clean(self):
+#         # Ensure that a question is associated with either a paper or a topic, but not both.
+#         if self.paper and self.topic:
+#             raise ValidationError("A question can only be associated with either a paper or a topic, not both.")
 
-
-# class Response(models.Model):
-#     question = models.ForeignKey(
-#         Question, on_delete=models.CASCADE, related_name='questions')
-#     response_text = models.TextField()
-
-#     def __str__(self):
-#         return self.question_text
-
-# Models for different types of questions
-
-
-# class Question(models.Model):
-#     # Course that the question belongs to
-#     paper = models.ForeignKey(Paper, on_delete=models.CASCADE, null=True)
-#     question_type = models.CharField(max_length=100, choices=[
-#         ('MCQ', 'Multiple Choice Question'),
-#         ('SAQ', 'Short Answer Question'),
-#         ('IVQ', 'Image/Video Question'),
-#     ])  # Type of the question (Multiple Choice, Short Answer, or Image/Video)
-#     question_text = models.TextField(null=True)  # Text of the question
-#     answer = models.TextField(blank=True)  # Answer to the question (optional)
-#     # File upload field for the question (optional)
-#     file = models.FileField(upload_to='image_video_questions/', blank=True)
+#     def save(self, *args, **kwargs):
+#         self.clean()  # Run validation before saving
+#         super().save(*args, **kwargs)
 
 #     def __str__(self):
-#         return self.question_text
+#         return f"{self.question_text}"
 
+class Attempt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+    # topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    is_timer_paused = models.BooleanField(default=False)
 
-# class MultipleChoiceQuestion(models.Model):
-#     text = models.TextField(null=True)  # Text of the question
-#     correct_answer = models.TextField(null=True)  # Correct answer to the question
-#     explanation = models.TextField(null=True)  # Explanation for the correct answer
-#     # Course that the question belongs to
-#     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    def start_timer(self):
+        self.start_time = timezone.now()
+        self.save()
 
-#     def __str__(self):
-#         return self.text
-# null=True
+    def pause_timer(self):
+        if self.start_time and not self.is_timer_paused:
+            self.end_time = timezone.now()
+            self.is_timer_paused = True
+            self.save()
 
-# class ShortAnswerQuestion(models.Model):
-#     text = models.TextField(null=True)  # Text of the question
-#     answer = models.TextField(null=True)  # Answer to the question
-#     # Discussion or additional information about the question (optional)
-#     discussion = models.TextField(blank=True)
-#     # Course that the question belongs to
-#     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    def resume_timer(self):
+        if self.start_time and self.is_timer_paused:
+            time_paused = self.end_time - self.start_time
+            self.start_time = timezone.now() - time_paused
+            self.end_time = None
+            self.is_timer_paused = False
+            self.save()
 
-#     def __str__(self):
-#         return self.text
+    def stop_timer(self):
+        if self.start_time:
+            self.end_time = timezone.now()
+            self.save()
 
-
-# class ImageVideoQuestion(models.Model):
-#     text = models.TextField(null=True)  # Text of the question
-#     # File upload field for the question
-#     file = models.FileField(upload_to='image_video_questions/', null=True)
-#     # Course that the question belongs to
-#     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
-
-#     def __str__(self):
-#         return self.text
-
-# # stores the answer given by a user to a particular question
-
-
-# class Answer(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-#     question = models.ForeignKey(
-#         Question, on_delete=models.CASCADE, related_name='answers')
-#     answer_text = models.TextField()
-
-#     def __str__(self):
-#         return f"{self.user.username} - {self.question.text}"
-# # stores the comment posted by a user to a particular question
-
-
-# class Comment(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     text = models.TextField()
-
-#     def __str__(self):
-#         return f"{self.user.username} - {self.question.text}"
-
-# # store the results of a user's attempt at a question
-
+    def get_elapsed_time(self):
+        if self.start_time and not self.is_timer_paused:
+            return timezone.now() - self.start_time
+        return None
 
 # class Result(models.Model):
 #     user = models.ForeignKey(User, on_delete=models.CASCADE)
